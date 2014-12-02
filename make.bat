e4c50cecaceba1b15dba105adaad136097495632
Peter Godwin <pgodwin@gmail.com>
2014-12-02 15:29:27 +1000

    Hacked up the Windows Build script to output to ./build

--- make.bat
+++ make.bat
@@ -21,41 +21,62 @@
 ::
 @echo off
 
-type src\namespaces.js >> pivotviewer.js
-type src\pubsub.js >> pivotviewer.js
-type src\utils.js >> pivotviewer.js
-type src\models.js >> pivotviewer.js
-type src\collectionloader.js >> pivotviewer.js
-type src\jsonloader.js >> pivotviewer.js
-type src\views\ipivotviewerview.js >> pivotviewer.js
-type src\views\tilebasedview.js >> pivotviewer.js
-type src\views\dataview.js >> pivotviewer.js
-type src\views\graphview.js >> pivotviewer.js
-type src\views\gridview.js >> pivotviewer.js
-type src\views\iimagecontroller.js >> pivotviewer.js
-type src\views\LoadImageSetHelper.js >> pivotviewer.js
-type src\views\mapview.js >> pivotviewer.js
-type src\views\mapview2.js >> pivotviewer.js
-type src\views\timeview.js >> pivotviewer.js
-type src\views\tableview.js >> pivotviewer.js
-type src\views\tilecontroller.js >> pivotviewer.js
-type src\views\deepzoom.js >> pivotviewer.js
-type src\views\simpleimage.js >> pivotviewer.js
-type src\pivotviewer.js >> pivotviewer.js
+mkdir build
+del build\* /s /q
+
+type src\namespaces.js >> build\pivotviewer.js
+type src\pubsub.js >> build\pivotviewer.js
+type src\utils.js >> build\pivotviewer.js
+type src\models.js >> build\pivotviewer.js
+type src\collectionloader.js >> build\pivotviewer.js
+type src\jsonloader.js >> build\pivotviewer.js
+type src\views\ipivotviewerview.js >> build\pivotviewer.js
+type src\views\tilebasedview.js >> build\pivotviewer.js
+type src\views\dataview.js >> build\pivotviewer.js
+type src\views\graphview.js >> build\pivotviewer.js
+type src\views\gridview.js >> build\pivotviewer.js
+type src\views\iimagecontroller.js >> build\pivotviewer.js
+type src\views\LoadImageSetHelper.js >> build\pivotviewer.js
+type src\views\mapview.js >> build\pivotviewer.js
+type src\views\mapview2.js >> build\pivotviewer.js
+type src\views\timeview.js >> build\pivotviewer.js
+type src\views\tableview.js >> build\pivotviewer.js
+type src\views\tilecontroller.js >> build\pivotviewer.js
+type src\views\deepzoom.js >> build\pivotviewer.js
+type src\views\simpleimage.js >> build\pivotviewer.js
+type src\pivotviewer.js >> build\pivotviewer.js
 
 copy pivotviewer.js pivotviewer.min.js
 
 ::
 ::  Make scripts folder
 ::
-mkdir scripts
-mkdir scripts\images
-mkdir scripts\timeline_js
-mkdir scripts\timeline_js\images
-copy lib\leaflet\images\* scripts\images\
-copy lib\wicket\wicket-arcgis.min.js scripts\
-copy lib\wicket\wicket-gmap3.min.js scripts\
-copy lib\wicket\wicket-leaflet.min.js scripts\
-copy lib\simile-timeline\timeline_js\images\blue-circle.png scripts\timeline_js\images\
-copy lib\simile-timeline\timeline_js\images\dark-red-circle.png scripts\timeline_js\images\
+mkdir build\scripts
+mkdir build\scripts\images
+mkdir build\scripts\timeline_js
+mkdir build\scripts\timeline_js\images
+copy lib\leaflet\images\* build\scripts\images\
+copy lib\wicket\wicket-arcgis.min.js build\scripts\
+copy lib\wicket\wicket-gmap3.min.js build\scripts\
+copy lib\wicket\wicket-leaflet.min.js build\scripts\
+copy lib\simile-timeline\timeline_js\images\blue-circle.png build\scripts\timeline_js\images\
+copy lib\simile-timeline\timeline_js\images\dark-red-circle.png build\scripts\timeline_js\images\
 
+::
+::  Make Lib Folder
+::
+mkdir build\lib
+xcopy lib build\lib /e
+
+
+::
+:: Make Images Folder
+::
+mkdir build\images
+copy images\* build\images
+
+::
+:: Copy defaults and other data
+::
+copy defaults build
+copy timeline.json build
